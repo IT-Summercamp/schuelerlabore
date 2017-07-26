ee3f70a45649bea732e80c9afe985979e200d85b
schulung04 <schulung04@mms-t-systems.de>
2017-07-26 14:58:46 +0200

    SchülerlaboreSachsen auf Startseite

--- resources/views/startseite.blade.php
+++ resources/views/startseite.blade.php
@@ -5,7 +5,7 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default" style="text-align:center">
-							<div class="panel-heading">Startseite</div>
+							<div class="panel-heading">SchülerlaboreSachsen</div>
 
 							<div class="panel-body">
 									<img src="header_image.png" style="width:100%;height:100%">
