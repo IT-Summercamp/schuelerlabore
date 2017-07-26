429b394d79b1069f57bf4824666ce234f9ab534e
Sven Liebert <mail@sven-liebert.de>
2017-07-26 21:38:08 +0200

    Fix

--- resources/views/startseite.blade.php
+++ resources/views/startseite.blade.php
@@ -12,8 +12,8 @@
 								<p><font size="3">Herzlich Willkommen bei den sächsischen Schülerlaboren, Orte zum Lernen, Experimentieren und staunen. Hier könnt ihr verschiedene Berufe kennenlernen und bekommt die Möglichkeit selbstständig zu forschen. Biologie, Physik und Chemie, aber auch Mathematik, Informatik und die Technikwissenschaften erscheinen hier nicht mehr als graue Theorie, sondern präsentieren sich als spannende Arbeitsfelder mit hohem Alltagsbezug. Die Schülerlabore ergänzen den Unterricht und eröffnen neue fachliche Perspektiven.  Auf dieser Seite könnt ihr euch über die Angebote der sächsischen Schülerlabore informieren und bei Interesse auch gleich anmelden.
 								</font></p>
 
-								<img src="/public/image/karte.PNG" style="width:100%;height:100%">
-								<img align="middle" src="/public/image/legende.JPG" style="width:100%;height:100%"></br>
+								<img src="/public/image/Karte.PNG" style="width:100%;height:100%">
+								<img src="/public/image/Legende.JPG" style="width:100%;height:100%"></br>
 								<a href="/public/schuelerlabore" class="btn btn-primary btn-block" role="button" style="background-color:#2ECC71">Erkunde</a>
 							</div>
 			</div>
