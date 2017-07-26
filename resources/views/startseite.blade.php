6d4229f8062f8e6661b697c4131584e2a0d85f99
schulung03 <schulung03@mms-t-systems.de>
2017-07-26 11:54:23 +0200

    Image2

--- resources/views/startseite.blade.php
+++ resources/views/startseite.blade.php
@@ -8,7 +8,7 @@
 							<div class="panel-heading">Startseite</div>
 
 							<div class="panel-body">
-									<img src="/public/header_image.png" style="width:100%;height:100%">
+									<img src="/public/img/header_image.png" style="width:100%;height:100%">
 								<p>Herzlich Willkommen bei den sächsischen Schülerlaboren, Orte zum Lernen, Experimentieren und staunen. <br />Hier könnt ihr verschiedene Berufe kennenlernen und bekommt die Möglichkeit selbstständig zu forschen. Biologie, Physik und Chemie, aber auch Mathematik, Informatik und die Technikwissenschaften erscheinen hier nicht mehr als graue Theorie, sondern präsentieren sich als spannende Arbeitsfelder mit hohem Alltagsbezug. <br />Die Schülerlabore ergänzen den Unterricht und eröffnen neue fachliche Perspektiven. <br /> Auf dieser Seite könnt ihr euch über die Angebote der sächsischen Schülerlabore informieren und bei Interesse auch gleich anmelden.</p>
 							</div>
 			</div>
