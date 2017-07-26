4cc5c445a7e8057f16274e727621f389930a76cf
schulung04 <schulung04@mms-t-systems.de>
2017-07-26 10:43:29 +0200

    Impressum & Startseite

--- resources/views/impressum.blade.php
+++ resources/views/impressum.blade.php
@@ -7,6 +7,45 @@
 			<div class="panel panel-default">
 
 				<div class="panel-heading">Impressum </div>
+				<!doctype html>
+				<html>
+
+				<body bgcolor="white">
+
+				<font color="black">
+
+				<h2>Verantwortlich als Urheber und Herausgeber:</h2>
+
+
+
+				<h2>Haftungsausschluss:</h2>
+				Wir bemühen uns, auf dieser Website richtige und vollständige Informationen zur Verfügung zu stellen.
+				Wir übernehmen jedoch keine Haftung oder Garantie für die Aktualität, Richtigkeit oder Vollständigkeit der auf dieser Seite bereitgestellten Informationen.
+				Dies gilt auch für alle Verbindungen (Hyperlinks), auf die diese Website direkt oder indirekt verweist.
+				Wir sind für den Inhalt einer Seite, die mit einem solchen Link erreicht wird, nicht verantwortlich.
+				</body>
+
+				<h2>Fotos:</h2>
+				    HZDR/André Forner,HS Mittweida/Stephan Floss, Gläsernes Labor/Sven Doering, Gläsernes Labor/Oliver Killing, ComLabBio/SBG, HTW Schülerlabor Chemie, Lernlabor Farbe/Kevin Appelhans, SchülerLab Berlin/Chris Gossmann, Netzwerk Teilchenwelt/Julia Socher, Schülerrechenzentrum/ Mathias Rauh, Lernort Albertinum, UFZ/ Sebstian Wiedling, Science meets School/ Thoma Kruse, Wunderland Physik, Hochschule Mittweida/Helmut Hammer, solaris/Wolfgang Thieme <p></p>
+				    Stand: September 2016<p></p>
+				    Logo: <p></p>
+				    <a href="https://pixabay.com/de/zahnr%C3%A4der-optionen-einstellungen-467261/">https://pixabay.com/de/zahnr%C3%A4der-optionen-einstellungen-467261/</a><p>
+				   <a href="https://static.vecteezy.com/system/resources/previews/000/119/138/non_2x/free-science-and-laboratory-icons-vector.jpg">https://static.vecteezy.com/system/resources/previews/000/119/138/non_2x/free-science-and-laboratory-icons-vector.jpg</a><br />
+
+				    <!-- Der Link hier muss nochmal umgeändert werden -->
+				   <a href="http://icon-icons.com/de/symbol/Taschenrechner/50442">http://icon-icons.com/de/symbol/Taschenrechner/50442</a> <br />
+
+
+				<div align="center">
+
+				    <h2>Creators:</h2>
+				    <img src="Platzhalter3.jpg"
+				     width="500"
+				     height="350"
+				     align="center"/><p></p>
+
+				Diese Website wurde innerhalb des IT-Summercamps 2017 entwickelt und dabei unterstützt von Communardo Software GmbH, T-Mulitmedia Systems und SAP SE
+				    </div>
 
 
 
