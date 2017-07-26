9b885c1c72efe49bda33ea3c9186e43584beb123
Niklas <schulung08@mms-t-systems.de>
2017-07-26 11:35:28 +0200

    Logo auf Startseite

--- resources/views/startseite.blade.php
+++ resources/views/startseite.blade.php
@@ -4,12 +4,13 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Startseite</div>
+			<div class="panel panel-default" style="text-align:center">
+							<div class="panel-heading">Startseite</div>
 
-				<div class="panel-body">
-					<p>Herzlich Willkommen bei den sächsischen Schülerlaboren, Orte zum Lernen, Experimentieren und staunen. <br />Hier könnt ihr verschiedene Berufe kennenlernen und bekommt die Möglichkeit selbstständig zu forschen. Biologie, Physik und Chemie, aber auch Mathematik, Informatik und die Technikwissenschaften erscheinen hier nicht mehr als graue Theorie, sondern präsentieren sich als spannende Arbeitsfelder mit hohem Alltagsbezug. <br />Die Schülerlabore ergänzen den Unterricht und eröffnen neue fachliche Perspektiven. <br /> Auf dieser Seite könnt ihr euch über die Angebote der sächsischen Schülerlabore informieren und bei Interesse auch gleich anmelden.</p>
-				</div>
+							<div class="panel-body">
+									<img src="header_image.png" style="width:100%;height:100%">
+								<p>Herzlich Willkommen bei den sächsischen Schülerlaboren, Orte zum Lernen, Experimentieren und staunen. <br />Hier könnt ihr verschiedene Berufe kennenlernen und bekommt die Möglichkeit selbstständig zu forschen. Biologie, Physik und Chemie, aber auch Mathematik, Informatik und die Technikwissenschaften erscheinen hier nicht mehr als graue Theorie, sondern präsentieren sich als spannende Arbeitsfelder mit hohem Alltagsbezug. <br />Die Schülerlabore ergänzen den Unterricht und eröffnen neue fachliche Perspektiven. <br /> Auf dieser Seite könnt ihr euch über die Angebote der sächsischen Schülerlabore informieren und bei Interesse auch gleich anmelden.</p>
+							</div>
 			</div>
 		</div>
 	</div>
