c8d3eecedea8fd533e0ae1108a2a324a7ce8203c
IT-Summercamp <Martin.Lehmann@t-systems.com>
2017-07-27 21:06:28 +0200

    Authoren Metadaten

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Entwickelt von Sven Siebert, Niklas Lang, Merlin Brandner">
 	<title> </title>
 	<style>
 	a {word-wrap: break-word;}
