bd096b18eb6783ccfc6058f7d629d3fe4db1bb7f
Sven Liebert <mail@sven-liebert.de>
2017-07-27 22:34:31 +0200

    Google Analytics eingefügt

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -92,6 +92,16 @@
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-103556341-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 	<footer class="footer">
       <div class="container">
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
