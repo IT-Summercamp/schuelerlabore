667931953099e0fa45ff6effd9e6fd824f1d8927
Niklas <schulung08@mms-t-systems.de>
2017-07-27 11:14:06 +0200

    Fix Adminfarbe

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -22,7 +22,7 @@
 	<![endif]-->
 </head>
 <body>
-	<nav class="navbar navbar-default"  style="background-color:##34495e;" >
+	<nav class="navbar navbar-default"  style="background-color:#34495e;" >
 		<div class="container-fluid">
 			<div class="navbar-header">
 				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
