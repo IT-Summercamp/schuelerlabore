014602655e9a607f6738ed64a3311c65bcd6ee5b
Sven Liebert <mail@sven-liebert.de>
2017-07-28 17:26:46 +0200

    Kleine Abänderungen

---
+++ resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Das Passwort muss 6 Zeichen beinhalten.",
+	"user" => "Keinen Benutzer mit dieser E-Mail gefunden.",
+	"token" => "Dieser Passwortlink ist ungültig..",
+	"sent" => "Wir haben Ihnen einen Link zum zurücksetzen zugesendet.",
+	"reset" => "Dein Passwort wurde zurückgesetzt!",
+
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,11 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
+				<div style="font-size : 180%;" class="panel-heading">Login</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<strong>Whoops!</strong> Etwas stimmt mit der Eingabe nicht.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
